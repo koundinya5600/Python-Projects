46f0d5917901fbc4741cd7c2b3974d45e8362632
koundinya5600 <62827775+koundinya5600@users.noreply.github.com>
2021-01-12 19:43:20 +0530

    Add files via upload

---
+++ WhatsappBOT.py
@@ -0,0 +1,3 @@
+import pywhatkit
+pywhatkit.sendwhatmsg('+918*********','Hello there! How are you?',00,0)
+#here you have to enter the whatsapp nuumber and time to send the message in hour and min format
